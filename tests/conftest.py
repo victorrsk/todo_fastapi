24b6a963ef7aecffbce6b7ba905f447da8e4e154
João Victor <jv191207@gmail.com>
2026-01-06 22:00:08 -0300

    hooks sqlalchemy & teste banco de dados

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,22 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from main.app import app
 from models.models_db import Base
 
 
+# fixture que retorna um client para testes de rotas da API
 @pytest.fixture
 def client():
     return TestClient(app)
 
 
+# fixture para criação de tabela e abertura de session para operações no bd
 @pytest.fixture
 def session():
     engine = create_engine('sqlite:///:memory:')
@@ -21,3 +26,22 @@ def session():
         yield session
 
     Base.metadata.drop_all(engine)
+
+
+# usado para inserir um created_at "falso" no bd
+@contextmanager
+def _mock_db_time(model, time=datetime(2026, 1, 1)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(target=model, identifier='before_insert', fn=fake_time_hook)
+
+    yield time
+
+    event.remove(target=model, identifier='before_insert', fn=fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,24 @@
+
 from sqlalchemy import select
 
 from models.models_db import User
 
 
-def test_create_user(session):
-    test_user = User(
-        username='teste', email='teste@email.com', password='teste123'
-    )
-    session.add(test_user)
-    session.commit()
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        test_user = User(
+            username='teste', email='teste@email.com', password='teste123'
+        )
+        session.add(test_user)
+        session.commit()
+
     user = session.scalar(select(User).where(User.username == 'teste'))
+    # a forma mais limpa de fazer isso seria usando asdict() do dataclass
+    # mas eu usei declarative base e não @mapped_as_dataclass no registry
+    # na criação do modelo
+    breakpoint()
+    assert user.id == 1
     assert user.username == 'teste'
+    assert user.email == 'teste@email.com'
+    assert user.password == 'teste123'
+    assert user.created_at == time
