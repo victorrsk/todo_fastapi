f0e0b8b38c0fb752bd41c04296e326a06880847a
João Victor <jv191207@gmail.com>
2026-01-08 23:51:34 -0300

    integração rotas get, post, put

--- main/app.py
+++ main/app.py
@@ -1,8 +1,12 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from database import get_session
+from models.models_db import User
 from schema.schemas import (
     HTML_HELLO,
     Message,
@@ -28,59 +32,74 @@ def hello():
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    # traz no máximo 10 registros
+    session: Session = Depends(get_session),
+    limit: int = 10,
+    offset: int = 0,
+):
+    users = session.scalars(select(User).offset(offset).limit(limit))
+    return {'users': users}
 
 
 @app.get(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def read_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(detail='user not found', status_code=404)
-    return database[user_id - 1]
+def read_user(user_id: int, session=Depends(get_session)):
 
+    user_db = session.scalar(select(User).where(User.id == user_id))
+    if user_db:
+        user = User(
+            id=user_db.id, username=user_db.username, email=user_db.email
+        )
+        return user
 
-# @app.get(
-# '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
-# )
-# def read_user(user_id: int):
-#    return database[user_id - 1]
+    raise HTTPException(detail='not found', status_code=HTTPStatus.NOT_FOUND)
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    from sqlalchemy import create_engine, select
-    from sqlalchemy.orm import Session
-    from models.models_db import User
-    from settings import Settings
-    from datetime import datetime
-    
-    engine = create_engine(Settings().DATABASE_URL)
-    session = Session(engine)
-    
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
     # básico
-    user_db = session.scalar(select(User).where(User.username == user.username))
+    user_db = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
     if user_db:
-        raise HTTPException(detail='user already exists', status_code=HTTPStatus.CONFLICT)
-
-    user_db = User(username=user.username, email=user.email, password=user.password) #created_at=datetime.utcnow(), updated_at=datetime.utcnow())
+        if user_db.username == user.username:
+            raise HTTPException(
+                detail='username already in use',
+                status_code=HTTPStatus.CONFLICT,
+            )
+        elif user_db.email == user.email:
+            raise HTTPException(
+                detail='email already in use', status_code=HTTPStatus.CONFLICT
+            )
+    user_db = User(
+        username=user.username, email=user.email, password=user.password
+    )
     session.add(user_db)
     session.commit()
     session.refresh(user_db)
     return user_db
 
+
+# FIXME inserir um id inválido ou email/username já existente ainda não foi
+# tratado corretamente
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema):
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='user not found'
-        )
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
-    return user_with_id
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    user_updt = session.scalar(select(User).where(User.id == user_id))
+    user_updt.username = user.username
+    user_updt.email = user.email
+    user_updt.password = user.password
+    session.add(user_updt)
+    session.commit()
+    session.refresh(user_updt)
+    return user_updt
 
 
 @app.delete('/users/{user_id}', status_code=HTTPStatus.OK)
